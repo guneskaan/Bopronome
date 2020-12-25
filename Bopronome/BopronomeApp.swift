448f2b4586595801411ef606e301d3ae1eb8a42f
Gunesh Pinar <gkpinar@uwaterloo.ca>
2020-12-25 11:27:45 +0300

    Initial Commit

---
+++ Bopronome/BopronomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BopronomeApp.swift
+//  Bopronome
+//
+//  Created by Gunesh Pinar on 2020-12-25.
+//
+
+import SwiftUI
+
+@main
+struct BopronomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
